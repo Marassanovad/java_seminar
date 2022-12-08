7ce6785fc1c092755e88c1a5aaf4539f1f016a5d
Marassanovad <maradasha@mail.ru>
2022-12-08 20:59:36 +0600

    HW seminar 5

--- java_seminar/src/Seminar5/TaskIntersectionofTwoArrays.java
+++ java_seminar/src/Seminar5/TaskIntersectionofTwoArrays.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 public class TaskIntersectionofTwoArrays {
-    public int [] interselection(int [] nums1, int [] nums2) {
+    public int[] interselection(int[] nums1, int[] nums2) {
         if (nums1.length > nums2.length) {
             return interselection(nums2, nums1);
         }
@@ -15,15 +15,15 @@ public class TaskIntersectionofTwoArrays {
             set.add(nums1[i]);
         }
         List<Integer> intersection = new ArrayList<>();
-        for (int num: nums2) {
+        for (int num : nums2) {
             if (set.contains(nums2[num])) {
                 intersection.add(nums2[num]);
                 set.remove(nums2[num]);
             }
         }
-        int [] outarr = new int [intersection.size()];
+        int[] outarr = new int[intersection.size()];
         for (int i = 0; i < outarr.length; i++) {
-            outarr[i]= intersection.get(i);
+            outarr[i] = intersection.get(i);
         }
         return outarr;
     }
